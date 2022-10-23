13adf00b2b4e59d676e84601e9315e28f2665e91
Zain Fathoni <me@zainf.dev>
2022-10-23 14:30:05 +0800

    feat: add widget

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,25 @@
-import { JSX } from "preact";
-import { IS_BROWSER } from "$fresh/runtime.ts";
+import { JSX } from 'preact'
+import { IS_BROWSER } from '$fresh/runtime.ts'
+import { classNames } from '../utils/class-names.tsx'
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
-  return (
-    <button
-      {...props}
-      disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border(gray-100 2) hover:bg-gray-200"
-    />
-  );
+export type ButtonProps = JSX.HTMLAttributes<HTMLButtonElement> & {
+  variant: 'primary' | 'secondary'
+  className?: string
+}
+
+const variantStyles = {
+  primary:
+    'bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70',
+  secondary:
+    'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70',
+}
+
+export function Button({ variant = 'primary', className, ...props }: ButtonProps) {
+  className = classNames(
+    'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
+    variantStyles[variant],
+    className ?? ''
+  )
+
+  return <button {...props} disabled={!IS_BROWSER || props.disabled} className={className} />
 }
