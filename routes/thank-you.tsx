1a0a60264b73c28f8e323564fceef600098c458e
Zain Fathoni <me@zainf.dev>
2023-02-17 23:42:58 +0800

    style(fmt): configure deno fmt to align with Prettier

--- routes/thank-you.tsx
+++ routes/thank-you.tsx
@@ -6,11 +6,11 @@ export default function ThankYou() {
     <>
       <Head>
         <title>You're subscribed - Zain Fathoni</title>
-        <meta name="description" content="Thanks for subscribing to my newsletter." />
+        <meta name='description' content='Thanks for subscribing to my newsletter.' />
       </Head>
       <SimpleLayout
-        title="Thanks for subscribing."
-        intro="I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings."
+        title='Thanks for subscribing.'
+        intro='I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings.'
       />
     </>
   )
